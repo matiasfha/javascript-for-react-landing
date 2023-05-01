e97453b8d52c9fb3de2630d5acb86b6b1bc830d3
Matias Hernandez <matiasfh@gmail.com>
2023-05-01 10:53:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fundamentos de Typescript
+
+Landing page para el curso Fundamentos de Typescript
+
+## Tech stack:
+
+Astro, React, Tailwind, Framer Motion
+
+
+
+##  How to run
+
+All commands are run from the root of the project, from a terminal:
+
+| Command                | Action                                             |
+| :--------------------- | :------------------------------------------------- |
+| `npm install`          | Installs dependencies                              |
+| `npm run dev`          | Starts local dev server at `localhost:3000`        |
+| `npm run build`        | Build your production site to `./dist/`            |
+| `npm run preview`      | Preview your build locally, before deploying       |
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        customPrimary: "rgb(99, 102, 241)",
+        customSecondary: "rgb(161, 163, 247)",
+        customDarkBg1: "rgb(31, 32, 35)",
+        customDarkBg2: "rgb(38, 39, 43)",
+        customDarkBg3: "rgb(48, 49, 54)",
+        customDarkBg3Hover: "rgb(55, 56, 62)",
+        customContentSubtitle: "rgb(178, 184, 205)",
+        customGrayBorder: "rgb(255,255,255,0.1)",
+        customGrayText: "rgb(174, 178, 183)",
+        customDarkBgTransparent: "rgb(31, 32, 35, 0.7)",
+        customDarkBgTransparentDarker: "rgb(0,0,0,0.5)",
+        customDarkBgTransparentLighter: "rgb(48, 49, 54, 0.7)",
+        darkPurple: "#4a1c37ff",
+        spaceCadet: "#332965ff",
+        caribbean: "#027478ff",
+        verdigris: "#0ab9c2ff",
+        aqua: "#09f0f9ff",
+        revolver: {
+          800: "#1f1222",
+          900: "#190f1b",
+        },
+      },
+
+      fontFamily: {
+        Inter: "Inter",
+      },
+      screens: {
+        xs: "530px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xll: "1400px",
+        "2xl": "1536px",
+      },
+    },
+  },
+};
